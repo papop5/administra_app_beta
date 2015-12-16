8326e500eb338dc7cbcb396c7ee81c68b11b13f1
Rafael Perez <rafper5@gmail.com>
2015-12-16 07:56:48 -0500

    Primer commit

---
+++ config/initializers/inflections.rb
@@ -0,0 +1,23 @@
+# Be sure to restart your server when you modify this file.
+
+# Add new inflection rules using the following format. Inflections
+# are locale specific, and you may define rules for as many different
+# locales as you wish. All of these examples are active by default:
+# ActiveSupport::Inflector.inflections(:en) do |inflect|
+#   inflect.plural /^(ox)$/i, '\1en'
+#   inflect.singular /^(ox)en/i, '\1'
+#   inflect.irregular 'person', 'people'
+#   inflect.uncountable %w( fish sheep )
+# end
+
+# These inflection rules are supported but not enabled by default:
+# ActiveSupport::Inflector.inflections(:en) do |inflect|
+#   inflect.acronym 'RESTful'
+# end
+ActiveSupport::Inflector.inflections(:en) do |inflect|
+   inflect.irregular  'residente', 'residentes'
+   inflect.irregular  'apartamento', 'apartamentos'
+   inflect.irregular  'vehiculo', 'vehiculos'
+end
+
+#esto sireve para definir singulares y plurares de la aplicacion, para que no se presenten problemas con el español
