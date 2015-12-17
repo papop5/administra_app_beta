cac08e33cccdac007eaa19d656a8c4a29bcc9c05
Rafael Perez <rafper5@gmail.com>
2015-12-17 08:07:28 -0500

    Valor de administracion, incremento de residentes

--- app/models/residente.rb
+++ app/models/residente.rb
@@ -1,3 +1,10 @@
 class Residente < ActiveRecord::Base
   belongs_to :apartamento
+  after_create :incrementar_residente
+
+	def incrementar_residente
+		apartamento.nro_habitantes += 1
+		apartamento.save
+	end
+
 end
